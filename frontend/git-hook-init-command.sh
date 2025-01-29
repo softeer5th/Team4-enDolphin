f15887965bfe027aeeab508a02449101e326fd09
dioo1461 <dioo1461@gmail.com>
2025-01-29 20:14:27 +0900

    feat: git hook의 path를 .github/hooks로 변경하도록 동작을 수정
    기존에는 .github/hooks 내의 파일들을 .git/hooks에 복사하는 동작이었음

--- frontend/git-hook-init-command.sh
+++
@@ -1,6 +0,0 @@
-cp ../.github/hooks/pre-commit ../.git/hooks/pre-commit
-chmod +x ../.git/hooks/pre-commit
-cp ../.github/hooks/commit-msg ../.git/hooks/commit-msg
-chmod +x ../.git/hooks/commit-msg
-cp ../.github/hooks/pre-push ../.git/hooks/pre-push
-chmod +x ../.git/hooks/pre-push
---
+++ frontend/src/scripts/git-hook-init-command.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git config --local core.hooksPath .github/hooks
+chmod +x ../.github/hooks
