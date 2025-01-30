169e8528d103cb2614d191234a44f900f2b8f384
efdao <efdao@naver.com>
2025-01-30 16:41:16 +0900

    feat: JwtProvider 클래스 추가
    jwt 토큰 발행, 검증 로직 구현

---
+++ backend/src/main/java/endolphin/backend/global/security/JwtProvider.java
@@ -0,0 +1,46 @@
+package endolphin.backend.global.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private final long validityInMs = 60 * 60 * 1000L; // 1시간
+    private final SecretKey key;
+
+    public JwtProvider(@Value("${jwt.secret}") String secretKey) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    // 토큰 발행
+    public String createToken(Long userId, String email) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + validityInMs);
+
+        return Jwts.builder()
+            .claim("userId", userId)
+            .claim("email", email)
+            .issuedAt(now)
+            .expiration(expiry)
+            .signWith(key)
+            .compact();
+    }
+
+    // 검증, 파싱
+    public Jws<Claims> validateToken(String token) {
+        try {
+            return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new RuntimeException("Invalid or expired JWT token");
+        }
+    }
+}
