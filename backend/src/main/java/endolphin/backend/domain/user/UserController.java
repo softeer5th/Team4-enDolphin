06378a949dbe08170d0dd56d8de9564a1b068de6
kwon204 <dafeg3@naver.com>
2025-01-31 17:18:46 +0900

    feat: UserController에 /api/v1 prefix 추가

--- backend/src/main/java/endolphin/backend/domain/user/UserController.java
+++ backend/src/main/java/endolphin/backend/domain/user/UserController.java
@@ -5,10 +5,12 @@ import endolphin.backend.domain.user.dto.UrlResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserController {
 
