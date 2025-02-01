6c8948ad81aaa7865ff3fd9c70e929c7766987f0
hamo-o <yheel369@gmail.com>
2025-02-02 05:16:59 +0900

    feat: 선택된 날의 년, 월, 해당일이 포함된 주의 날들, 몇째 주인지 반환하는 함수 작성

---
+++ frontend/src/utils/date.ts
@@ -0,0 +1,53 @@
+const isStartWithFirstWeek = (firstDay: Date) => {
+  const day = firstDay.getDay();
+  if (day === 5 || day == 6 || day === 0) return false;
+  return true;
+};
+
+// TODO: 테스트 코드 추가, 겹치는 코드 리팩터링
+const calcWeekNum = (date: Date) => {
+  const weekMap: Record<string, string> = {
+    1: '첫째주',
+    2: '둘째주',
+    3: '셋째주',
+    4: '넷째주',
+    5: '다섯째주',
+  };
+
+  const isFirstDay = date.getDate() === 1;
+  if (isFirstDay && !isStartWithFirstWeek(date)) {
+    const prevMonthLastDate = new Date(date.getFullYear(), date.getMonth(), 0);
+    const prevMonthLastDay = prevMonthLastDate.getDay();
+    const daysInPrevMonth = prevMonthLastDate.getDate();
+
+    const lastWeekDays = 7 - prevMonthLastDay;
+    return Math.ceil((daysInPrevMonth - lastWeekDays + 7) / 7);
+  }
+
+  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  const firstWeekfirstDay = 
+    isStartWithFirstWeek(date) ? firstDay.getDate() : firstDay.getDate() + 1;
+  const week = Math.ceil((date.getDate() - firstWeekfirstDay + 7) / 7);
+
+  return weekMap[week];
+};
+
+export const formatDateToWeek = (date: Date) => {
+  const selected = new Date(date);
+  const firstDateOfWeek = new Date(selected.setDate(selected.getDate() - selected.getDay())); 
+  const year = date.getFullYear().toString();
+
+  const days = new Array(7).fill(0)
+    .map((_, i)=>{
+      const DAY = 60 * 60 * 24 * 1000;
+      const date = new Date(firstDateOfWeek.getTime() + i * DAY);
+      return date.getDate();
+    });
+
+  return {
+    year: year.slice(2),
+    month: date.getMonth() + 1,
+    week: calcWeekNum(date),
+    days,
+  };
+};
