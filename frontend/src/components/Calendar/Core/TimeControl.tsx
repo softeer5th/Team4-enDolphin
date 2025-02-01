450e697e2466c24e3f25013ce675317d4aeb12e8
hamo-o <yheel369@gmail.com>
2025-02-02 05:58:39 +0900

    fix: 불필요한 input 제거

--- frontend/src/components/Calendar/Core/TimeControl.tsx
+++ frontend/src/components/Calendar/Core/TimeControl.tsx
@@ -7,18 +7,14 @@ export const TimeControl = () => {
     handleClickToday, 
     handleClickPrevWeek, 
     handleClickNextWeek,
-    handleChangeWeek, 
   } = useCalendarContext();
   const { year, month, week  } = formatDateToWeek(selected);
+  const weekString = `${year}년 ${month}월 ${week}`;
 
   return  (
     <div>
       <button onClick={handleClickPrevWeek}>{'<'}</button>
-      <input
-        onChange={(e) => handleChangeWeek(new Date(e.target.value))}
-        type='text'
-        value={`${year}년 ${month}월 ${week}`}
-      />
+      <span>{weekString}</span>
       <button onClick={handleClickNextWeek}>{'>'}</button>
       {/* TODO: 버튼 컴포넌트 완성 시 교체 */}
       <button onClick={handleClickToday}>오늘</button>
