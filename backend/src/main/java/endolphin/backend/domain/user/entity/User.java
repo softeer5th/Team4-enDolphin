8d15252fd4f15b1e66d20d81932d60a76c947327
efdao <efdao@naver.com>
2025-02-01 13:49:15 +0900

    chore: user_id 칼럼을 id로 변경

--- backend/src/main/java/endolphin/backend/domain/user/entity/User.java
+++ backend/src/main/java/endolphin/backend/domain/user/entity/User.java
@@ -19,7 +19,6 @@ public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private Long id;
 
     @Column(nullable = false)
