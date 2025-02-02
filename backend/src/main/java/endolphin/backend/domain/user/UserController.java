fdeaf9694a6ffb72c430a3ece994708270be6ee0
kwon204 <dafeg3@naver.com>
2025-02-02 09:45:03 +0900

    fix: UserController 구글 로그인 callback url 수정

--- backend/src/main/java/endolphin/backend/domain/user/UserController.java
+++ backend/src/main/java/endolphin/backend/domain/user/UserController.java
@@ -5,18 +5,16 @@ import endolphin.backend.domain.user.dto.UrlResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/google")
+    @GetMapping("/api/v1/google")
     public ResponseEntity<UrlResponse> loginUrl() {
         UrlResponse response = userService.getGoogleLoginUrl();
         return ResponseEntity.ok(response);
