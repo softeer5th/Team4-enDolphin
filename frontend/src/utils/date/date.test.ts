a339c6795f35aac438cd866a4638bb48a9703e64
hamo-o <yheel369@gmail.com>
2025-02-03 17:58:32 +0900

    test: formatDateToWeek함수 1일이 금, 토, 일인 경우 테스트

---
+++ frontend/src/utils/date/date.test.ts
@@ -0,0 +1,17 @@
+import { WEEK_MAP } from '@/constants/date';
+
+import { formatDateToWeek } from '.';
+
+describe('formatDateToWeek 함수', ()=>{
+  describe('1일이 금, 토, 일요일인 경우', ()=>{
+    test('1일은 전 달 마지막 주차로 판단한다.', ()=> {
+      const { week } = formatDateToWeek(new Date('2025-02-01'));
+      expect(week).toBe(WEEK_MAP[5]);
+    });
+
+    test('2일은 1주차로 판단한다.', ()=> {
+      const { week } = formatDateToWeek(new Date('2025-02-02'));
+      expect(week).toBe(WEEK_MAP[1]);
+    });
+  });
+});
--- frontend/src/utils/date/index.ts
+++ frontend/src/utils/date/index.ts
@@ -1,5 +1,10 @@
 import { WEEK_MAP } from '@/constants/date';
 
+/**
+ * 해당 달의 1일이 첫째주에 포함되는지를 판단합니다.
+ * @param firstDay - 해당 달의 1일 날짜 객체.
+ * @returns - 해당 달의 1일이 해당 달의 첫째주인지 여부.
+ */
 const isStartWithFirstWeek = (firstDay: Date) => {
   const day = firstDay.getDay();
   if (day === 5 || day == 6 || day === 0) return false;
@@ -7,12 +12,13 @@ const isStartWithFirstWeek = (firstDay: Date) => {
 };
 
 /**
- * 
+ * 날짜가 해당 달의 몇 번째 주인지 계산합니다.
+ * @example calcWeekNum(new Date(2025, 1, 1)); // 1월 5주차
+ * @example calcWeekNum(new Date(2025, 1, 2)); // 2월 1주차
  * @param date - 날짜 객체.
- * @returns 
+ * @returns - 해당 날짜가 몇 번째 주인지를 알리는 문자열 정보. ex) 첫째주, 둘째주, ...
  */
 const calcWeekNum = (date: Date): string => {
-
   const isFirstDay = date.getDate() === 1;
   if (isFirstDay && !isStartWithFirstWeek(date)) {
     const prevMonthLastDate = new Date(date.getFullYear(), date.getMonth(), 0);
@@ -42,8 +48,7 @@ type DateWeekType = {
 /**
  * 
  * @param date - 날짜 객체.
- * @returns 해당 날짜가 포함된 주의 정보를 반환.
- * 
+ * @returns - 특정 날짜의 년, 월, 주, 날짜가 포함된 주의 날짜 객체 배열.
  */
 export const formatDateToWeek = (date: Date): DateWeekType => {
   const selected = new Date(date);
