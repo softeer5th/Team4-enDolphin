00e559dff2b931f514da720fff6e8fb005a81975
hamo-o <yheel369@gmail.com>
2025-02-04 02:46:30 +0900

    test: 1일이 첫주차가 아닐 때, 그 이전 달의 마지막 주는 그대로 판단해야 함

--- frontend/src/utils/date/date.test.ts
+++ frontend/src/utils/date/date.test.ts
@@ -2,10 +2,12 @@ import { WEEK_MAP } from '@/constants/date';
 
 import { formatDateToWeek } from '.';
 
+// TODO: 테스트 파일의 경우 max-len만 해제
+// eslint-disable-next-line
 describe('formatDateToWeek 함수', ()=>{
   describe('1일이 금, 토, 일요일인 경우', ()=>{
     describe('1일이 토요일인 경우', ()=>{
-      test('1일은 전 달 마지막 주차로 판단한다.', ()=> {
+      test('1일은 전 달 마지막 주차로 판단한다.', () => {
         const { month, week } = formatDateToWeek(new Date('2025-02-01'));
         expect(month).toBe(1);
         expect(week).toBe(WEEK_MAP[5]);
@@ -15,7 +17,13 @@ describe('formatDateToWeek 함수', ()=>{
         expect(month).toBe(1);
         expect(week).toBe(WEEK_MAP[5]);
       });
-    });
+      test.each(['27', '28', '29', '30', '31'])(
+        '전 달의 마지막 주차는 그대로 판단한다.', (date)=> {
+          const { month, week } = formatDateToWeek(new Date(`2025-01-${date}`));
+          expect(month).toBe(1);
+          expect(week).toBe(WEEK_MAP[5]);
+        });
+    }); 
   });
   describe('1일이 월, 화, 수, 목요일인 경우', ()=>{
     describe('1일이 목요일인 경우', ()=>{
