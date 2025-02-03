514f97e18b5fa9242e96113f83952de4adfc4e42
hamo-o <yheel369@gmail.com>
2025-02-03 21:51:46 +0900

    test: 달 검증 추가

--- frontend/src/utils/date/date.test.ts
+++ frontend/src/utils/date/date.test.ts
@@ -6,11 +6,13 @@ describe('formatDateToWeek 함수', ()=>{
   describe('1일이 금, 토, 일요일인 경우', ()=>{
     describe('1일이 토요일인 경우', ()=>{
       test('1일은 전 달 마지막 주차로 판단한다.', ()=> {
-        const { week } = formatDateToWeek(new Date('2025-02-01'));
+        const { month, week } = formatDateToWeek(new Date('2025-02-01'));
+        expect(month).toBe(1);
         expect(week).toBe(WEEK_MAP[5]);
       });
       test('2일은 전 달 마지막 주차로 판단한다.', ()=> {
-        const { week } = formatDateToWeek(new Date('2025-02-02'));
+        const { month, week } = formatDateToWeek(new Date('2025-02-02'));
+        expect(month).toBe(1);
         expect(week).toBe(WEEK_MAP[5]);
       });
     });
@@ -18,15 +20,18 @@ describe('formatDateToWeek 함수', ()=>{
   describe('1일이 월, 화, 수, 목요일인 경우', ()=>{
     describe('1일이 목요일인 경우', ()=>{
       test('전 달의 마지막 주차는 이번 달의 첫째주로 판단한다.', ()=>{
-        const { week } = formatDateToWeek(new Date('2024-07-31'));
+        const { month, week } = formatDateToWeek(new Date('2024-07-31'));
+        expect(month).toBe(8);
         expect(week).toBe(WEEK_MAP[1]);
       });
       test('1일은 첫째주로 판단한다.', ()=> {
-        const { week } = formatDateToWeek(new Date('2024-08-01'));
+        const { month, week } = formatDateToWeek(new Date('2024-08-01'));
+        expect(month).toBe(8);
         expect(week).toBe(WEEK_MAP[1]);
       });
       test('2일은 첫째주로 판단한다.', ()=> {
-        const { week } = formatDateToWeek(new Date('2024-08-02'));
+        const { month, week } = formatDateToWeek(new Date('2024-08-02'));
+        expect(month).toBe(8);
         expect(week).toBe(WEEK_MAP[1]);
       });
     });
