92bb73a7a1b44a33102384b5faa3328027f16bf0
hamo-o <yheel369@gmail.com>
2025-02-03 16:16:17 +0900

    feat: vite config test 및 alias 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,28 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vitest/config';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(dirname, 'src'),
+      '@components': path.resolve(dirname, 'src/components'),
+      '@hooks': path.resolve(dirname, 'src/hooks'),
+      '@utils': path.resolve(dirname, 'src/utils'),
+      '@pages': path.resolve(dirname, 'src/pages'),
+      '@theme': path.resolve(dirname, 'src/theme'),
+    },
+  },
+  test: {
+    globals: true,
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+    environment: 'jsdom',
+  },
 });
