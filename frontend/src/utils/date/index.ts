7fb240cf8705ac0a7a01bb27a72998cf2dcafee9
hamo-o <yheel369@gmail.com>
2025-02-04 02:00:34 +0900

    fix: 몇주차인지 구하는 함수만 분리하지 않고 년, 월, 주차를 함께 반환하는 함수와 주차 정보를 반환하는 함수 분리

--- frontend/src/utils/date/index.ts
+++ frontend/src/utils/date/index.ts
@@ -28,43 +28,51 @@ const isEndWithLastWeek = (date: Date) => {
   return true;
 };
 
-/**
- * 날짜가 해당 달의 몇 번째 주인지 계산합니다.
- * @example calcWeekNum(new Date(2025, 1, 1)); // 1월 5주차
- * @example calcWeekNum(new Date(2025, 1, 2)); // 2월 1주차
- * @param date - 날짜 객체.
- * @returns - 해당 날짜가 몇 번째 주인지를 알리는 문자열 정보. ex) 첫째주, 둘째주, ...
- */
-const calcWeekNum = (date: Date): string => {
-  if (!isStartWithFirstWeek(date)) {
-    const prevMonthLastDate = new Date(date.getFullYear(), date.getMonth(), 0);
-    return calcWeekNum(prevMonthLastDate);
-  }
-  if (!isEndWithLastWeek(date)) return WEEK_MAP[1];
-
-  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-  const firstWeekFirstDay = firstDay.getDate();
-  const week = Math.floor((date.getDate() - firstWeekFirstDay + 7) / 7);
-  return WEEK_MAP[week];
-};
-
 type DateWeekType = {
   year: string;
   month: number;
   week: string;
-  dates: Date[];
+};
+/**
+ * 날짜가 몇년 몇월 몇 번째 주인지 계산합니다.
+ * @example formatDateToWeek(new Date(2025, 2, 1)); // 25년 1월 다섯째주
+ * @example formatDateToWeek(new Date(2025, 2, 2)); // 25년 2월 첫째주
+ * @param date - 날짜 객체.
+ * @returns - 특정 날짜의 년, 월, 주 정보를 담은 객체.
+ */
+export const formatDateToWeek = (date: Date): DateWeekType => {
+  if (!isStartWithFirstWeek(date)) {
+    const prevMonthLastDate = new Date(date.getFullYear(), date.getMonth(), 0);
+    return formatDateToWeek(prevMonthLastDate);
+  }
+  
+  if (!isEndWithLastWeek(date)) {
+    const nextMonthFirstDate = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+    return {
+      year: String(nextMonthFirstDate.getFullYear()).slice(2),
+      month: nextMonthFirstDate.getMonth() + 1,
+      week: WEEK_MAP[1],
+    };
+  }
+
+  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  const firstWeekFirstDay = firstDay.getDate();
+  const week = Math.floor((date.getDate() - firstWeekFirstDay + 7) / 7);
+  return {
+    year: String(date.getFullYear()).slice(2),
+    month: date.getMonth() + 1,
+    week: WEEK_MAP[week],
+  };
 };
 
 /**
  * 
  * @param date - 날짜 객체.
- * @returns - 특정 날짜의 년, 월, 주, 날짜가 포함된 주의 날짜 객체 배열.
+ * @returns - 특정 날짜가 포함된 주의 날짜 객체 배열.
  */
-export const formatDateToWeek = (date: Date): DateWeekType => {
+export const formatDateToWeekDates = (date: Date): Date[] => {
   const selected = new Date(date);
   const firstDateOfWeek = new Date(selected.setDate(selected.getDate() - selected.getDay())); 
-  const year = date.getFullYear().toString();
-
   const dates = new Array(7).fill(0)
     .map((_, i)=>{
       const DAY = 60 * 60 * 24 * 1000;
@@ -72,12 +80,7 @@ export const formatDateToWeek = (date: Date): DateWeekType => {
       return date;
     });
 
-  return {
-    year: year.slice(2),
-    month: date.getMonth() + 1,
-    week: calcWeekNum(date),
-    dates,
-  };
+  return dates;
 };
 
 /**
