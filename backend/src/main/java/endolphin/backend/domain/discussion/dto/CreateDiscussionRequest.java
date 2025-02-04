ed4744caacb09cabdf7139f49131535857711170
efdao <efdao@naver.com>
2025-02-04 11:35:14 +0900

    feat: CreateDiscussionRequest validation 일부 추가

--- backend/src/main/java/endolphin/backend/domain/discussion/dto/CreateDiscussionRequest.java
+++ backend/src/main/java/endolphin/backend/domain/discussion/dto/CreateDiscussionRequest.java
@@ -1,6 +1,9 @@
 package endolphin.backend.domain.discussion.dto;
 
 import endolphin.backend.domain.discussion.enums.MeetingMethod;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
@@ -12,9 +15,9 @@ public record CreateDiscussionRequest(
     @NotNull LocalDate dateRangeEnd,
     @NotNull LocalTime timeRangeStart,
     @NotNull LocalTime timeRangeEnd,
-    @NotNull Integer duration,
+    @NotNull @Min(30) @Max(180) Integer duration,
     MeetingMethod meetingMethod,
     String location,
-    @NotNull LocalDate deadline) {
+    @NotNull @Future LocalDate deadline) {
 
 }
