188838403e03bd63f765e460906dfba358022d19
hamo-o <yheel369@gmail.com>
2025-02-04 20:06:02 +0900

    feat: 날짜 객체를 시간 문자열로 바꿔주는 유틸함수 작성

---
+++ frontend/src/utils/date/time.ts
@@ -0,0 +1,8 @@
+export const formatDateToTimeString = (date: Date): string => {
+  const hours = date.getHours().toString()
+    .padStart(2, '0');
+  const minutes = date.getMinutes().toString()
+    .padStart(2, '0');
+
+  return `${hours}:${minutes}`;
+};
