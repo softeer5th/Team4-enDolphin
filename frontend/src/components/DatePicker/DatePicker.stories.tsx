0a217280b278633725c448403caaa565e8ee56d4
이재영 <dioo1461@gmail.com>
2025-02-06 03:22:30 +0900

    feat: storybook에서 calendarType 선택할 수 있도록 구현

--- frontend/src/components/DatePicker/DatePicker.stories.tsx
+++ frontend/src/components/DatePicker/DatePicker.stories.tsx
@@ -5,7 +5,11 @@ import DatePicker from '.';
 const meta: Meta = {
   title: 'Calendar/DatePicker',
   component: DatePicker,
-  args: {
+  argTypes: {
+    calendarType: {
+      control: { type: 'radio' },
+      options: ['select', 'range'],
+    },
   },
 } satisfies Meta<typeof DatePicker>;
 
@@ -14,8 +18,7 @@ export default meta;
 type Story = StoryObj<typeof DatePicker>;
 
 export const Default: Story = {
-  render: () => 
-    <DatePicker
-      calendarType='range'
-    />,
+  args: {
+    calendarType: 'select',
+  },  
 };
