85724008cf959f0407c994f2e9d107f6fdae591f
efdao <efdao@naver.com>
2025-02-05 13:58:00 +0900

    feat: ErrorResponse에 httpStatus 추가

--- backend/src/main/java/endolphin/backend/global/error/ErrorResponse.java
+++ backend/src/main/java/endolphin/backend/global/error/ErrorResponse.java
@@ -1,12 +1,19 @@
 package endolphin.backend.global.error;
 
 import endolphin.backend.global.error.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.http.HttpStatus;
 
+@Schema(description = "에러 응답")
 public record ErrorResponse(
+    @Schema(description = "상태코드", example = "500")
+    HttpStatus httpStatus,
+    @Schema(description = "에러 메시지", example = "Internal Server Error")
     String message,
+    @Schema(description = "에러 코드", example = "C001")
     String code
 ) {
     public static ErrorResponse of(ErrorCode errorCode) {
-        return new ErrorResponse(errorCode.getMessage(), errorCode.getCode());
+        return new ErrorResponse(errorCode.getHttpStatus(), errorCode.getMessage(), errorCode.getCode());
     }
 }
