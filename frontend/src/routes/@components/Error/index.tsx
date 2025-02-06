add8371e44fd14339b8538c98c151fc3663d4af4
hamo-o <yheel369@gmail.com>
2025-02-07 04:50:47 +0900

    feat: 이미지 webp 변환, 에러페이지 마크업 마무리

--- frontend/src/routes/@components/Error/index.tsx
+++ frontend/src/routes/@components/Error/index.tsx
@@ -9,15 +9,22 @@ const Error = () => (
   <Flex
     align='center'
     direction='column'
-    gap={300}
+    gap={700}
+    height='100vh'
   >
     <img
       height={180}
-      src='/images/assets/error.png'
+      src='/images/assets/error.webp'
       width={180}
     />
-    <Text color={vars.color.Ref.Netural[800]} typo='h3'>유효하지 않은 링크입니다.</Text>
-    <Text color={vars.color.Ref.Netural[500]} typo='b2M'>링크가 삭제되거나 변경되었어요.</Text>
+    <Flex
+      align='center'
+      direction='column'
+      gap={300}
+    >
+      <Text color={vars.color.Ref.Netural[800]} typo='h3'>유효하지 않은 링크입니다.</Text>
+      <Text color={vars.color.Ref.Netural[500]} typo='b2M'>링크가 삭제되거나 변경되었어요.</Text>
+    </Flex>
     <Button
       as={Link}
       size='lg'
