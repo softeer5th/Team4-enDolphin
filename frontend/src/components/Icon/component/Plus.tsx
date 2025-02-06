dba6c4c4662f3a37a480bc9e6aaae6d5c17d1199
hamo-o <yheel369@gmail.com>
2025-02-06 11:59:26 +0900

    feat: Plus 아이콘 추가

---
+++ frontend/src/components/Icon/component/Plus.tsx
@@ -0,0 +1,13 @@
+   
+import type { IconProps } from '../Icon.d.ts';
+
+export const Plus = ({ clickable = false, className, width = 24 , stroke = "white", ...rest }: IconProps) => {
+    return (
+        <svg width={width} height={width} viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg" aria-label="plus icon" fill="none" className={className} style={{ cursor: clickable ? "pointer": "default", ...rest.style }} {...rest}>
+<path d="M12 4.5V19.5M19.5 12H4.5" stroke={stroke} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+
+    );
+};
+
+Plus.displayName = 'Plus';
