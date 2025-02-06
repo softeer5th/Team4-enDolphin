a3a2463972131ea9a21a1df342abce6b30d94540
kwon204 <dafeg3@naver.com>
2025-02-06 10:51:06 +0900

    refactor: Filter Configuration을 FilterConfig.java로 모음

---
+++ backend/src/main/java/endolphin/backend/global/config/FilterConfig.java
@@ -0,0 +1,29 @@
+package endolphin.backend.global.config;
+
+import endolphin.backend.global.logging.LogFilter;
+import endolphin.backend.global.security.JwtAuthFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<LogFilter> logFilterFilterRegistrationBean() {
+        FilterRegistrationBean<LogFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtAuthFilter> jwtAuthFilterRegistration(JwtAuthFilter filter) {
+        FilterRegistrationBean<JwtAuthFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/*");
+        registration.setOrder(2);
+        return registration;
+    }
+}
