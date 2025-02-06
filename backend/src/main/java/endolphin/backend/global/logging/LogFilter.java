49606ed383636babbf821961a8358ce2b0bc7402
kwon204 <32539398+kwon204@users.noreply.github.com>
2025-02-06 14:17:44 +0900

    [BE-Feat] 로그 세팅 추가 (#97)
    * feat: logback-spring.xml 파일 설정
    
    * refactor: Filter Configuration을 FilterConfig.java로 모음
    
    * feat: LogFilter 추가
    
    요청 URI, Method, 응답 시간 로깅
    
    * feat: 응답 시간을 nanoTime으로 계산, 요청 쿼리 스트링도 출력하도록 추가
    
    * feat: 로그를 비동기로 처리하는 코드 추가
    
    * refactor: 허용하는 url 적용방식 수정

---
+++ backend/src/main/java/endolphin/backend/global/logging/LogFilter.java
@@ -0,0 +1,36 @@
+package endolphin.backend.global.logging;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+public class LogFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
+        long startTime = System.nanoTime();
+        String correlationId = UUID.randomUUID().toString();
+        MDC.put("correlationId", correlationId);
+        log.info("Incoming Request: [{}] {} {}",
+            request.getMethod(), request.getRequestURI(),
+            request.getQueryString() != null ? request.getQueryString() : "");
+
+        filterChain.doFilter(request, response);
+
+        long endTime = System.nanoTime();
+        log.info("Incoming Response: [{}] {}, {}, {} ms",
+            request.getMethod(), request.getRequestURI() ,
+            response.getStatus(), (endTime - startTime) / 1_000_000.0);
+
+        MDC.clear();
+    }
+}
