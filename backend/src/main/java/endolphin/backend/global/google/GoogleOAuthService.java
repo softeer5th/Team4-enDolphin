b2bdf54315905cafdeb15a271f8484fe2df9b025
kwon204 <dafeg3@naver.com>
2025-02-07 10:27:41 +0900

    refactor: RestTemplate -> RestClient로 변경
    RestTemplate와 RestClient는 같은 기능을 하지만 메소드 체이닝 방식으로 사용할 수 있어서 교체

--- backend/src/main/java/endolphin/backend/global/google/GoogleOAuthService.java
+++ backend/src/main/java/endolphin/backend/global/google/GoogleOAuthService.java
@@ -4,49 +4,21 @@ import endolphin.backend.domain.user.dto.GoogleTokens;
 import endolphin.backend.domain.user.dto.GoogleUserInfo;
 import endolphin.backend.global.config.GoogleOAuthProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Service
 @RequiredArgsConstructor
 public class GoogleOAuthService {
+
     private final GoogleOAuthProperties googleOAuthProperties;
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     public GoogleTokens getGoogleTokens(String code) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        HttpEntity<MultiValueMap<String, String>> request = getHttpEntity(
-            code, headers);
-        ResponseEntity<GoogleTokens> response = restTemplate.postForEntity(
-            googleOAuthProperties.tokenUrl(), request,
-            GoogleTokens.class);
-
-        return response.getBody();
-    }
-
-    public GoogleUserInfo getUserInfo(String accessToken) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<GoogleUserInfo> result = restTemplate.exchange(googleOAuthProperties.userInfoUrl(),
-            HttpMethod.GET, entity, GoogleUserInfo.class);
-
-        return result.getBody();
-    }
-
-    private HttpEntity<MultiValueMap<String, String>> getHttpEntity(String code,
-        HttpHeaders headers) {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("client_id", googleOAuthProperties.clientId());
         params.add("client_secret", googleOAuthProperties.clientSecret());
@@ -55,9 +27,19 @@ public class GoogleOAuthService {
         params.add("grant_type", "authorization_code");
         params.add("access_type", "offline");
 
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
-        return request;
+        return restClient.post()
+            .uri(googleOAuthProperties.tokenUrl())
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(params)
+            .retrieve()
+            .body(GoogleTokens.class);
     }
 
-
+    public GoogleUserInfo getUserInfo(String accessToken) {
+        return restClient.get()
+            .uri(googleOAuthProperties.userInfoUrl())
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+            .retrieve()
+            .body(GoogleUserInfo.class);
+    }
 }
