3c8e570b59fd1ed029915d72c304fa7b06111577
hamo-o <yheel369@gmail.com>
2025-02-09 08:19:01 +0900

    feat: Dropdown 트리거 및 옵션 이벤트에 따라 열고 닫기, width CSS 변경

--- frontend/src/components/Dropdown/DropdownContext.ts
+++ frontend/src/components/Dropdown/DropdownContext.ts
@@ -4,6 +4,7 @@ interface DropdownContextProps {
   controlId: string;
   selectedValue: string;
   onChange: ((value: string) => void);
+  setIsOpen: ((isOpen: boolean) => void);
 }
 
 /**
@@ -12,5 +13,6 @@ interface DropdownContextProps {
  * @param controlId - Dropdown 컴포넌트의 id.
  * @param selectedValue - Dropdown 컴포넌트의 선택된 값.
  * @param onChange - Dropdown 컴포넌트의 값 변경 함수.
+* @param setIsOpen - Dropdown 컴포넌트의 오픈 상태 변경 함수.
  */
 export const DropdownContext = createContext<DropdownContextProps | null>(null);
