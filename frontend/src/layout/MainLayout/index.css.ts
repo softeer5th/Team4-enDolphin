8ec3d3a202a159e5de101be95aa6189c3d327aff
dioo1461 <dioo1461@gmail.com>
2025-02-08 18:27:22 +0900

    feat: 뷰포트 width > 1288일 때, main layout이 horizontal margin을 가지도록 구현

--- frontend/src/layout/MainLayout/index.css.ts
+++ frontend/src/layout/MainLayout/index.css.ts
@@ -1,11 +1,9 @@
 import { style } from '@vanilla-extract/css';
 
-import { vars } from '@/theme/index.css';
-
 export const containerStyle = style({
   width: '100vw',
   maxWidth: 1288,
   minHeight: '100vh',
-  overflowX: 'scroll',
-  padding: `56px ${vars.spacing[700]}`,
+  margin: '0 auto',
+  padding: '56px 0px',
 });
