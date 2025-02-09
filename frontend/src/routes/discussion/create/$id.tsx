ed39310aa3fbedf9b8fe35430873fa2ea760689d
hamo-o <yheel369@gmail.com>
2025-02-09 12:32:20 +0900

    chore: 변경된 폴더구조에 맞게 이동

--- frontend/src/routes/discussion/create/$id.tsx
+++ frontend/src/routes/discussion/create/$id.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import DiscussionCreateFinishPage from '../@components';
+import DiscussionCreateFinishPage from '@/pages/DiscussionCreateFinishPage';
 
 export const Route = createFileRoute('/discussion/create/$id')({
   component: DiscussionCreateFinishPage,
