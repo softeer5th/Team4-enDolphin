0e8fecef6edfc920dfeaba48f078efa9a66b323c
efdao <efdao@naver.com>
2025-02-11 00:36:04 +0900

    feat: CalendarRepository에 findByCalendarId() 추가

--- backend/src/main/java/endolphin/backend/domain/calendar/CalendarRepository.java
+++ backend/src/main/java/endolphin/backend/domain/calendar/CalendarRepository.java
@@ -1,10 +1,12 @@
 package endolphin.backend.domain.calendar;
 
 import endolphin.backend.domain.calendar.entity.Calendar;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
 
+    Optional<Calendar> findByCalendarId(String calendarId);
 }
