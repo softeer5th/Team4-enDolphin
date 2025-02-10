eee299462399dd980d8a252ff2b49d8c994e6946
efdao <efdao@naver.com>
2025-02-11 00:38:13 +0900

    feat: CalendarService에 메서드 추가
    1. syncToken 관련 메서드
    
    2. setWebhookProperties
    
    3. createCalendar

--- backend/src/main/java/endolphin/backend/domain/calendar/CalendarService.java
+++ backend/src/main/java/endolphin/backend/domain/calendar/CalendarService.java
@@ -1,8 +1,14 @@
 package endolphin.backend.domain.calendar;
 
+import endolphin.backend.domain.calendar.entity.Calendar;
 import endolphin.backend.domain.user.entity.User;
-import endolphin.backend.global.google.GoogleCalendarService;
+import endolphin.backend.global.error.exception.CalendarException;
+import endolphin.backend.global.error.exception.ErrorCode;
 import endolphin.backend.global.google.dto.GoogleCalendarDto;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +19,52 @@ import org.springframework.transaction.annotation.Transactional;
 public class CalendarService {
 
     private final CalendarRepository calendarRepository;
-    private final GoogleCalendarService googleCalendarService;
 
-    public void getAllEvents(GoogleCalendarDto calendar, User user) {
-        //TODO 캘린더 DB에 저장
+    public void createCalendar(GoogleCalendarDto calendar, User user) {
+        Calendar newCalendar = Calendar.builder()
+            .calendarId(calendar.id())
+            .user(user)
+            .name(calendar.summary())
+            .description(calendar.description())
+            .build();
+        calendarRepository.save(newCalendar);
+    }
 
-        googleCalendarService.getCalendarEvents(calendar.id(), user);
+    @Transactional(readOnly = true)
+    public String getSyncToken(String calendarId) {
+        Calendar calendar = calendarRepository.findByCalendarId(calendarId).orElseThrow(
+            () -> new CalendarException(ErrorCode.CALENDAR_NOT_FOUND_ERROR));
+        return calendar.getSyncToken();
+    }
 
-        //TODO personalEventService 호출해서 저장
+    public void setWebhookProperties(String calendarId, String resourceId, String channelId,
+        String expiration) {
+        Calendar calendar = calendarRepository.findByCalendarId(calendarId).orElseThrow(
+            () -> new CalendarException(ErrorCode.CALENDAR_NOT_FOUND_ERROR));
+
+        DateTimeFormatter formatter = DateTimeFormatter.RFC_1123_DATE_TIME;
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(expiration, formatter);
+        LocalDateTime expirationDateTime = zonedDateTime
+            .withZoneSameInstant(ZoneId.of("Asia/Seoul"))
+            .toLocalDateTime();
+
+        calendar.setWebhookProperties(resourceId, channelId, expirationDateTime);
+        calendarRepository.save(calendar);
+    }
+
+    public void updateSyncToken(String calendarId, String newSyncToken) {
+        Calendar calendar = calendarRepository.findByCalendarId(calendarId).orElseThrow(
+            () -> new CalendarException(ErrorCode.CALENDAR_NOT_FOUND_ERROR));
+        calendar.setSyncToken(newSyncToken);
+        calendarRepository.save(calendar);
+    }
+
+    public void clearSyncToken(String calendarId) {
+        Calendar calendar = calendarRepository.findByCalendarId(calendarId).orElseThrow(
+            () -> new CalendarException(ErrorCode.CALENDAR_NOT_FOUND_ERROR));
+        calendar.setSyncToken(null);
+        calendarRepository.save(calendar);
     }
 
 }
