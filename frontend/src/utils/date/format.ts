023d9d1fc0aa9b8e90f95a05cb9c5c0cec2540c3
hamo-o <yheel369@gmail.com>
2025-02-11 11:10:55 +0900

    feat: Date 객체를 YYYY.MM.DD 형태로 포맷팅 해주는 유틸 작성, 자주 사용하는 함수 분리

--- frontend/src/utils/date/format.ts
+++ frontend/src/utils/date/format.ts
@@ -1,11 +1,18 @@
+import { getYearMonthDay } from './date';
+
 /**
  * 날짜 객체를 YY-MM-DD 형식의 문자열로 변환합니다.
  * @param date - 날짜 객체
  * @returns 
  */
-export const formatDateToString = (date: Date): string => {
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+export const formatDateToBarString = (date: Date | null): string => {
+  if (!date) return '';
+  const { year, month, day } = getYearMonthDay(date);
   return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+};
+
+export const formatDateToDotString = (date: Date | null): string => {
+  if (!date) return '';
+  const { year, month, day } = getYearMonthDay(date);
+  return `${year}. ${month.toString().padStart(2, '0')}. ${day.toString().padStart(2, '0')}`;
 };
