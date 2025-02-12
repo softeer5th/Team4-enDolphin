b94bc2eadb57c15ed65883f9b1957778b8614e42
efdao <efdao@naver.com>
2025-02-13 00:49:53 +0900

    feat: Discussion entity에 password 칼럼 추가

--- backend/src/main/java/endolphin/backend/domain/discussion/entity/Discussion.java
+++ backend/src/main/java/endolphin/backend/domain/discussion/entity/Discussion.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
 @Getter
@@ -51,10 +52,14 @@ public class Discussion extends BaseTimeEntity {
     @Column(nullable = false)
     private LocalDate deadline;
 
+    @Setter
     @Enumerated(EnumType.STRING)
     @ColumnDefault("'ONGOING'")
     @Column(name = "status", nullable = false)
     private DiscussionStatus discussionStatus;
+
+    @Setter
+    private String password;
 
     @Builder
     public Discussion(String title, LocalDate dateStart, LocalDate dateEnd,
