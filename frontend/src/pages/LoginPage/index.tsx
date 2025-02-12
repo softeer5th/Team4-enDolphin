ff03bb2d78be89feb3b60eca6b1d17e2ec5db5ca
이재영 <dioo1461@gmail.com>
2025-02-13 00:38:26 +0900

    feat: 로그인 페이지 구현

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,8 +1,13 @@
 import { useMutation } from '@tanstack/react-query';
 import { useTransition } from 'react';
 
-import Button from '@/components/Button';
+import { Flex } from '@/components/Flex';
+import { Modal } from '@/components/Modal';
+import { Text } from '@/components/Text';
 import { requestGoogleLoginUrl } from '@/features/login/api';
+import { vars } from '@/theme/index.css';
+
+import { backdropStyle, googleLoginButtonStyle } from './index.css';
 
 const LoginPage = () => {
   const [isRedirectPending, startRedirectTransition] = useTransition();
@@ -17,14 +22,44 @@ const LoginPage = () => {
 
   const onGoogleLoginButtonClick = async () => {
     googleLogin.mutate();
-    // console.log(await requestGoogleLoginUrl());
   };
 
   return (
-    <div>
-      <Button onClick={onGoogleLoginButtonClick}>구글로그인</Button>
+    <div className={backdropStyle}>
+      <Modal
+        description={`Google 계정으로 간편하게 가입하고, 팀 프로젝트 일정 관리와 협업을 시작해보세요. 
+        ’언제만나?’는 대학생을 위한 최고의 팀플 관리 도구입니다!`}
+        isOpen={true} 
+        subTitle='로그인/회원가입'
+        title='언제만나 시작하기'
+      >
+        <Modal.Footer>
+          <GoogleLoginButton disabled={isRedirectPending} onClick={onGoogleLoginButtonClick} />
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
+
+const GoogleLoginButton = ({ onClick, disabled }: { onClick: () => void; disabled: boolean }) => (
+  <Flex
+    align='center'
+    as='button'
+    className={googleLoginButtonStyle}
+    disabled={disabled}
+    gap={200}
+    justify='center'
+    onClick={onClick}
+    width='full'
+  >
+    <img
+      alt='Google 로그인 아이콘'
+      height='20px'
+      src='src/components/Icon/png/google-login-icon.png'
+      width='20px'
+    />
+    <Text color={vars.color.Ref.Netural[400]} typo='t3'>Google로 시작하기</Text>
+  </Flex>
+);
 
 export default LoginPage;
--- frontend/src/routes/login/index.lazy.tsx
+++ frontend/src/routes/login/index.lazy.tsx
@@ -1,9 +1,14 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 
+import GlobalNavBar from '@/layout/GlobalNavBar';
+import { LoginLink } from '@/layout/GlobalNavBar/buttons';
 import LoginPage from '@/pages/LoginPage';
 
 const Login = () => (
   <>
+    <GlobalNavBar>
+      <LoginLink />
+    </GlobalNavBar>
     <LoginPage />
   </>
 );
