c87d3385dd0a86d33e12b71e66ef43d2057c36c6
hamo-o <yheel369@gmail.com>
2025-02-13 14:50:56 +0900

    refactor: 캘린더 카드 사이즈 반환 함수 컴포넌트 외부 분리

--- frontend/src/features/my-calendar/ui/CalendarCardList/index.tsx
+++ frontend/src/features/my-calendar/ui/CalendarCardList/index.tsx
@@ -8,38 +8,36 @@ interface DateRange {
   endDate: Date | null;
 }
 
-export const CalendarCardList = ({ cards }: { cards: DateRange[] }) => {
-  const calcSize = (height: number) => {
-    if (height < TIME_HEIGHT) return 'sm';
-    if (height < TIME_HEIGHT * 2.5) return 'md';
-    return 'lg';
-  };
-  
-  return (
-    <>
-      {cards.map((card, idx) => {
-        const { x: sx, y: sy } = calcPositionByDate(card.startDate);
-        const { y: ey } = calcPositionByDate(card.endDate);
-        const height = ey - sy;
-          
-        return (
-          <CalendarCard
-            endTime={card.endDate}
-            key={idx}
-            size={calcSize(height)}
-            startTime={card.startDate}
-            status='adjustable'
-            style={{
-              width: 'calc((100% - 72px) / 7)',
-              height,
-              position: 'absolute',
-              left: `calc(((100% - 72px) / 7 * ${sx}) + 72px)`,
-              top: 16 + sy,
-            }}
-            title='새 일정 제목이 개길면 어떻게 될까?'
-          />
-        );
-      })}
-    </>
-  );
+const calcSize = (height: number) => {
+  if (height < TIME_HEIGHT) return 'sm';
+  if (height < TIME_HEIGHT * 2.5) return 'md';
+  return 'lg';
 };
+
+export const CalendarCardList = ({ cards }: { cards: DateRange[] }) => (
+  <>
+    {cards.map((card, idx) => {
+      const { x: sx, y: sy } = calcPositionByDate(card.startDate);
+      const { y: ey } = calcPositionByDate(card.endDate);
+      const height = ey - sy;
+          
+      return (
+        <CalendarCard
+          endTime={card.endDate}
+          key={idx}
+          size={calcSize(height)}
+          startTime={card.startDate}
+          status='adjustable'
+          style={{
+            width: 'calc((100% - 72px) / 7)',
+            height,
+            position: 'absolute',
+            left: `calc(((100% - 72px) / 7 * ${sx}) + 72px)`,
+            top: 16 + sy,
+          }}
+          title='새 일정 제목이 개길면 어떻게 될까?'
+        />
+      );
+    })}
+  </>
+);
