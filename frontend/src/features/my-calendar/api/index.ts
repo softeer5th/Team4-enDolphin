45a858dffa665992ff7840836be6292a1867f999
hamo-o <yheel369@gmail.com>
2025-02-14 10:29:01 +0900

    fix: API URI 변경 및 파라미터 형식 변경

--- frontend/src/features/my-calendar/api/index.ts
+++ frontend/src/features/my-calendar/api/index.ts
@@ -6,8 +6,11 @@ export const personalEventApi = {
   getPersonalEvents: async (
     { startDateTime, endDateTime }: Pick<PersonalEventDTO, 'startDateTime' | 'endDateTime'>,
   ): Promise<PersonalEventResponse> => {
-    const response = await request.get('/api/v1/personal-events', {
-      params: { startDateTime, endDateTime },
+    const response = await request.get('/api/v1/personal-event', {
+      params: { 
+        startDateTime: `${startDateTime}T00:00:00`, 
+        endDateTime: `${endDateTime}T00:00:00`,
+      },
     });
     return response.data;
   },
