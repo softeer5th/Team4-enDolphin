46bf38c6aa9c5634e410d770080dbb620d8a1507
hamo-o <yheel369@gmail.com>
2025-02-16 13:26:42 +0900

    fix: 로그인 로직 두번 실행되지 않도록 수정

--- frontend/src/routes/oauth.redirect/index.tsx
+++ frontend/src/routes/oauth.redirect/index.tsx
@@ -5,16 +5,16 @@ import { useJWTMutation } from '@/features/login/api/mutations';
 import { getLastRoutePath } from '@/utils/route';
 
 const Redirect = () => {
-  const { loginMutate, isPending } = useJWTMutation();
+  const { loginMutate } = useJWTMutation();
   const lastPath = getLastRoutePath();
   const params: { code: string } = useSearch({ from: '/oauth/redirect/' });
   const { code } = params;
 
   useEffect(() => {
-    if (code && !isPending) {
+    if (code) {
       loginMutate({ code, lastPath });
     }
-  }, [code, loginMutate, isPending, lastPath]);
+  }, [code, loginMutate, lastPath]);
 
   return null;
 };
