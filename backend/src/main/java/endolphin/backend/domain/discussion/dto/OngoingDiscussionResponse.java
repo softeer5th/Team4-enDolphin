21a60cafc9bca1c70904ca936b7dacd93b5f2cc8
efdao <efdao@naver.com>
2025-02-16 23:56:49 +0900

    feat: OngoingDiscussionResponse dto 추가

---
+++ backend/src/main/java/endolphin/backend/domain/discussion/dto/OngoingDiscussionResponse.java
@@ -0,0 +1,14 @@
+package endolphin.backend.domain.discussion.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record OngoingDiscussionResponse(
+    Long discussionId,
+    String title,
+    LocalDate dateRangeStart,
+    LocalDate dateRangeEnd,
+    List<String> participantPictureUrls
+) {
+
+}
