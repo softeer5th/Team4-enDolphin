8d078850b8fcddeff1039774c6b380a2189223d8
hamo-o <yheel369@gmail.com>
2025-02-18 07:47:05 +0900

    fix: 불필요한 props 삭제

--- frontend/src/pages/DiscussionPage/index.tsx
+++ frontend/src/pages/DiscussionPage/index.tsx
@@ -18,13 +18,7 @@ const DiscussionPage = () =>(
       height='calc(100vh - 112px - 96px)'
       width='100%'
     >
-      <DiscussionMemberCheckbox members={[
-        { id: 1, name: '김동권', email: 'a@naver.com', picture: 'https://picsum.photos/200' },
-        { id: 2, name: '김동현', email: 'b@naver.com', picture: 'https://picsum.photos/200' },
-        { id: 3, name: '이재영', email: 'c@naver.com', picture: 'https://picsum.photos/200' },
-        { id: 4, name: '이현영', email: 'd@naver.com', picture: 'https://picsum.photos/200' },
-      ]}
-      />
+      <DiscussionMemberCheckbox />
       <DiscussionTab />
     </Flex>
   </Flex>
