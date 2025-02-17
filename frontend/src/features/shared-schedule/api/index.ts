2bc20c9f9a3c424f4d4e5120f981462d3e90d0dd
이재영 <dioo1461@gmail.com>
2025-02-17 19:03:09 +0900

    feat: response body 데이터 검증 추가

--- frontend/src/features/shared-schedule/api/index.ts
+++ frontend/src/features/shared-schedule/api/index.ts
@@ -1,25 +1,49 @@
 import { request } from '@/utils/fetch';
 
-import type { OngoingQueryType } from '../model/finishedSchedules';
+import type { OngoingSchedulesResponse, UpcomingSchedulesResponse } from '../model';
+import { 
+  FinishedSchedulesResponseSchema, 
+  OngoingSchedulesResponseSchema,
+  UpcomingSchedulesResponseSchema, 
+} from '../model';
+import type { FinishedSchedulesResponse, OngoingQueryType } from '../model/finishedSchedules';
 
 const ENDPOINT_PREFIX = '/api/v1/schedules';
 
 export const schedulesApi = {
-  getUpcomingSchedules: async () => request.get(ENDPOINT_PREFIX + '/upcoming'),
-  getOngoingSchedules: async (page: number, size: number, type: OngoingQueryType) => 
-    request.get(ENDPOINT_PREFIX + '/ongoing', {
+  getUpcomingSchedules: async (): Promise<UpcomingSchedulesResponse> => {
+    const response = await request.get(ENDPOINT_PREFIX + '/upcoming');
+    const validData = UpcomingSchedulesResponseSchema.parse(response.data);
+    return validData;
+  },
+  getOngoingSchedules: async (
+    page: number,
+    size: number,
+    type: OngoingQueryType,
+  ): Promise<OngoingSchedulesResponse> => {
+    const response = await request.get(ENDPOINT_PREFIX + '/ongoing', {
       params: {
         page: page.toString(),
         size: size.toString(),
         type: type,
       },
-    }),
-  getFinishedSchedules: async (page: number, size: number, year: number) => 
-    request.get(ENDPOINT_PREFIX + '/finished', {
+    });
+    const validData = OngoingSchedulesResponseSchema.parse(response.data);
+    return validData;
+  },
+  getFinishedSchedules: async (
+    page: number,
+    size: number,
+    year: number,
+  ): Promise<FinishedSchedulesResponse> => {
+    const response = await request.get(ENDPOINT_PREFIX + '/finished', {
       params: {
         page: page.toString(),
         size: size.toString(),
         year: year.toString(),
       },
-    }),
+    });
+    const validData = FinishedSchedulesResponseSchema.parse(response.data);
+    return validData;
+  },
 };
