858c0cc2c0419c9d03d92f29e462d7630f3d8ba3
efdao <efdao@naver.com>
2025-02-19 00:02:54 +0900

    feat: validate 메서드 추가

--- backend/src/main/java/endolphin/backend/global/util/Validator.java
+++ backend/src/main/java/endolphin/backend/global/util/Validator.java
@@ -1,5 +1,6 @@
 package endolphin.backend.global.util;
 
+import endolphin.backend.domain.discussion.entity.Discussion;
 import endolphin.backend.global.error.exception.ApiException;
 import endolphin.backend.global.error.exception.ErrorCode;
 import java.time.LocalDate;
@@ -10,19 +11,37 @@ public class Validator {
 
     public static void validateDateTimeRange(LocalDateTime start, LocalDateTime end) {
         if (end.isBefore(start)) {
-            throw new ApiException(ErrorCode.INVALID_DATE_TIME_RANGE);
+            throw new ApiException(ErrorCode.INVALID_DATE_TIME_REQUEST);
         }
     }
 
     public static void validateDateTimeRange(LocalDate start, LocalDate end) {
         if (end.isBefore(start)) {
-            throw new ApiException(ErrorCode.INVALID_DATE_TIME_RANGE);
+            throw new ApiException(ErrorCode.INVALID_DATE_TIME_REQUEST);
         }
     }
 
     public static void validateDateTimeRange(LocalTime start, LocalTime end) {
         if (end.isBefore(start)) {
-            throw new ApiException(ErrorCode.INVALID_DATE_TIME_RANGE);
+            throw new ApiException(ErrorCode.INVALID_DATE_TIME_REQUEST);
+        }
+    }
+
+    public static void validateInRange(Discussion discussion, LocalDateTime eventStart, LocalDateTime eventEnd) {
+        LocalDate startDate = discussion.getDateRangeStart();
+        LocalDate endDate = discussion.getDateRangeEnd();
+        LocalTime startTime = discussion.getTimeRangeStart();
+        LocalTime endTime = discussion.getTimeRangeEnd();
+
+        if (eventStart.toLocalDate().isBefore(startDate) || eventEnd.toLocalDate().isAfter(endDate)
+            || eventStart.toLocalTime().isBefore(startTime) || eventEnd.toLocalTime().isAfter(endTime)) {
+            throw new ApiException(ErrorCode.INVALID_DATE_TIME_REQUEST);
+        }
+    }
+
+    public static void validateDuration(LocalDateTime start, LocalDateTime end, int duration) {
+        if (!start.plusMinutes(duration).equals(end)) {
+            throw new ApiException(ErrorCode.INVALID_DATE_TIME_REQUEST);
         }
     }
 }
