3623faab490ed61e26c76c641bb09a979267a501
hamo-o <yheel369@gmail.com>
2025-02-19 07:22:18 +0900

    feat: 일정이 다른 날에 걸쳐있을 때 카드 두개로 나누기

--- frontend/src/features/my-calendar/ui/CalendarCardList/index.tsx
+++ frontend/src/features/my-calendar/ui/CalendarCardList/index.tsx
@@ -11,33 +11,62 @@ const calcSize = (height: number) => {
   return 'lg';
 };
 
+const DefaultCard = (
+  { card, start, end }: { card: PersonalEventResponse; start: Date; end: Date },
+) => {
+  const { x: sx, y: sy } = calcPositionByDate(start);
+  const { y: ey } = calcPositionByDate(end);
+  const height = ey - sy;
+  return (
+    <CalendarCard
+      calendarId={card.calendarId}
+      endTime={new Date(card.endDateTime)}
+      id={card.id}
+      key={card.id}
+      size={calcSize(height)}
+      startTime={new Date(card.startDateTime)}
+      status={card.isAdjustable ? 'adjustable' : 'fixed'}
+      style={{
+        width: 'calc((100% - 72px) / 7)',
+        height,
+        position: 'absolute',
+        left: `calc(((100% - 72px) / 7 * ${sx}) + 72px)`,
+        top: 16 + sy,
+      }}
+      title={card.title}
+    />
+  );
+};
+
 export const CalendarCardList = ({ cards }: { cards: PersonalEventResponse[] }) => (
   <>
     {cards.filter((card) => !isAllday(card.startDateTime, card.endDateTime))
       .map((card) => {
         const start = new Date(card.startDateTime);
         const end = new Date(card.endDateTime);
-        const { x: sx, y: sy } = calcPositionByDate(start);
-        const { y: ey } = calcPositionByDate(end);
-        const height = ey - sy;
+
+        if (start.getDate() !== end.getDate()) {
+          return (
+            <>
+              <DefaultCard
+                card={card}
+                end={new Date(start.getFullYear(), start.getMonth(), start.getDate(), 23, 59)}
+                start={start}
+              />
+              <DefaultCard
+                card={card}
+                end={end}
+                start={new Date(end.getFullYear(), end.getMonth(), end.getDate(), 0, 0)}
+              />
+            </>
+          );
+        }
 
         return (
-          <CalendarCard
-            calendarId={card.calendarId}
-            endTime={end}
-            id={card.id}
-            key={card.id}
-            size={calcSize(height)}
-            startTime={start}
-            status={card.isAdjustable ? 'adjustable' : 'fixed'}
-            style={{
-              width: 'calc((100% - 72px) / 7)',
-              height,
-              position: 'absolute',
-              left: `calc(((100% - 72px) / 7 * ${sx}) + 72px)`,
-              top: 16 + sy,
-            }}
-            title={card.title}
+          <DefaultCard
+            card={card}
+            end={end}
+            start={start}
           />
         );
       })}
