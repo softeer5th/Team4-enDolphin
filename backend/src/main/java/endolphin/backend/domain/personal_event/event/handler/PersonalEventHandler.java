718cb1ec5a0244457d56983f587122d30fcac2df
efdao <efdao@naver.com>
2025-02-20 12:11:59 +0900

    feat: syncWithGoogleEvents 수행후 sse로 변경사항 알림

--- backend/src/main/java/endolphin/backend/domain/personal_event/event/handler/PersonalEventHandler.java
+++ backend/src/main/java/endolphin/backend/domain/personal_event/event/handler/PersonalEventHandler.java
@@ -4,7 +4,10 @@ import endolphin.backend.domain.personal_event.PersonalEventService;
 import endolphin.backend.domain.user.entity.User;
 import endolphin.backend.global.google.dto.GoogleEvent;
 import endolphin.backend.global.google.event.GoogleEventChanged;
+import endolphin.backend.global.sse.SseEmitters;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,12 +16,15 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PersonalEventHandler {
     private final PersonalEventService personalEventService;
+    private final SseEmitters sseEmitters;
 
     @EventListener(classes = {GoogleEventChanged.class})
     public void sync(GoogleEventChanged event) {
         List<GoogleEvent> events = event.events();
         User user = event.user();
         String googleCalendarId = event.googleCalendarId();
-        personalEventService.syncWithGoogleEvents(events, user, googleCalendarId);
+        Set<LocalDate> changedDates = personalEventService.syncWithGoogleEvents(events, user, googleCalendarId);
+
+        sseEmitters.sendToUser(user.getId(), changedDates);
     }
 }
