32dd755d0810bd9f25d38fd4c073c80f0cb84b01
hamo-o <yheel369@gmail.com>
2025-02-20 11:09:29 +0900

    fix: 세그먼트컨트롤 바뀐 스펙에 맞게 수정

--- frontend/src/features/discussion/ui/DiscussionRank/index.tsx
+++ frontend/src/features/discussion/ui/DiscussionRank/index.tsx
@@ -7,10 +7,6 @@ import { useDiscussionRankQuery } from '../../api/queries';
 import { segmentControlContentsStyle, segmentControlStyle } from './index.css';
 import { RankContents } from './RankContents';
 
-const segmentOptions = [
-  { label: '참가자 많은 순', value: 'participant' },
-  { label: '빠른 시간 순', value: 'time' },
-];
 const DiscussionRank = () => {
   const params: { id: string } = useParams({ from: '/_main/discussion/$id' });
   const members = useMemberContext();
@@ -19,17 +15,22 @@ const DiscussionRank = () => {
       selectedUserIdList: members?.formState.checkedList || null, 
     });
 
+  const segmentOptions = [
+    { label: '참가자 많은 순', value: 'participant' },
+    { label: '빠른 시간 순', value: 'time' },
+  ];
+
   return (
     <SegmentControl
       className={segmentControlStyle}
-      defaultValue='참가자 많은 순'
+      defaultValue='participant'
       segmentOptions={segmentOptions}
       style='weak'
     >
-      <SegmentControl.Content className={segmentControlContentsStyle} value='참가자 많은 순'>
+      <SegmentControl.Content className={segmentControlContentsStyle} value='participant'>
         {!isLoading && <RankContents data={rank?.eventsRankedDefault || []} />}
       </SegmentControl.Content>
-      <SegmentControl.Content className={segmentControlContentsStyle} value='빠른 시간 순'>
+      <SegmentControl.Content className={segmentControlContentsStyle} value='time'>
         {!isLoading && <RankContents data={rank?.eventsRankedOfTime || []} />}
       </SegmentControl.Content>
     </SegmentControl>
