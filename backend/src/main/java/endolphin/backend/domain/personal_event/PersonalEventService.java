34419fe49d8bab252353e4d8beff5fce95e4cf49
kwon204 <dafeg3@naver.com>
2025-02-20 21:39:10 +0900

    fix: 비교를 equals로 변경

--- backend/src/main/java/endolphin/backend/domain/personal_event/PersonalEventService.java
+++ backend/src/main/java/endolphin/backend/domain/personal_event/PersonalEventService.java
@@ -277,7 +277,7 @@ public class PersonalEventService {
     }
 
     private boolean haveToSync(PersonalEvent personalEvent, PersonalEventRequest newEvent) {
-        return isChanged(personalEvent, newEvent) || personalEvent.getTitle() != newEvent.title();
+        return isChanged(personalEvent, newEvent) || !personalEvent.getTitle().equals(newEvent.title());
     }
 
     @Transactional(readOnly = true)
