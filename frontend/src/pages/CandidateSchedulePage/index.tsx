27748f9a10e5878e64560dcba7b260b9476a7c9e
이재영 <dioo1461@gmail.com>
2025-02-20 15:32:06 +0900

    fix: 후보 상세 페이지에서 라우트 이동(뒤로가기) 시에 query가 실행되던 문제 해결

--- frontend/src/pages/CandidateSchedulePage/index.tsx
+++ frontend/src/pages/CandidateSchedulePage/index.tsx
@@ -1,18 +1,22 @@
-import { useLocation, useParams } from '@tanstack/react-router';
+import { useParams } from '@tanstack/react-router';
 
 import CandidateScheduleDetail from '@/features/candidate-schedule/ui';
 
 import { backdropStyle } from './index.css';
 
-const CandidateSchedulePage = () => {
+const CandidateSchedulePage = (candidate: {
+  adjustCount: number;
+  startDateTime: string;
+  endDateTime: string;
+  selectedParticipantIds: number[];
+}) => {
   const { id } = useParams({ from: '/_main/discussion/candidate/$id' });
-  const { state } = useLocation();
-  const { candidate } = state ?? {};
   // TODO: candidate가 없을 경우에 대한 예외 처리
+
   return (
     <>
       <div className={backdropStyle} />
-      <CandidateScheduleDetail discussionId={Number(id)} {...candidate!} />
+      <CandidateScheduleDetail discussionId={Number(id)} {...candidate} />
     </>
   ); 
 };
--- frontend/src/routes/_main/discussion/candidate/$id.lazy.tsx
+++ frontend/src/routes/_main/discussion/candidate/$id.lazy.tsx
@@ -1,14 +1,20 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
+import { createLazyFileRoute, useLocation } from '@tanstack/react-router';
 
 import GlobalNavBar from '@/layout/GlobalNavBar';
 import CandidateSchedulePage from '@/pages/CandidateSchedulePage';
 
-const CandidateSchedule = () => (
-  <>
-    <GlobalNavBar></GlobalNavBar>
-    <CandidateSchedulePage />
-  </>
-);
+const CandidateSchedule = () => {
+  const { state } = useLocation();
+  const { candidate } = state ?? {};
+  if (!candidate) return <div>candidate is undefined or null</div>;
+
+  return (
+    <>
+      <GlobalNavBar></GlobalNavBar>
+      <CandidateSchedulePage {...candidate} />
+    </>
+  ); 
+};
 
 export const Route = createLazyFileRoute('/_main/discussion/candidate/$id')({
   component: CandidateSchedule,
