b9e1b732cfbc881cbee6b6bae0067d15f6715e57
Kim DongHyun <150654090+efdao@users.noreply.github.com>
2025-02-20 18:03:02 +0900

    [BE-Hotfix] 디버그용 로그 추가 (#238)

--- backend/src/main/java/endolphin/backend/domain/personal_event/event/handler/PersonalEventHandler.java
+++ backend/src/main/java/endolphin/backend/domain/personal_event/event/handler/PersonalEventHandler.java
@@ -9,22 +9,31 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class PersonalEventHandler {
     private final PersonalEventService personalEventService;
     private final SseEmitters sseEmitters;
 
     @EventListener(classes = {GoogleEventChanged.class})
     public void sync(GoogleEventChanged event) {
-        List<GoogleEvent> events = event.events();
-        User user = event.user();
-        String googleCalendarId = event.googleCalendarId();
-        Set<LocalDate> changedDates = personalEventService.syncWithGoogleEvents(events, user, googleCalendarId);
+        log.info("Syncing personal events with Google events");
+        try {
+            List<GoogleEvent> events = event.events();
+            User user = event.user();
+            log.info("Syncing personal events for user {}", user.getId());
+            String googleCalendarId = event.googleCalendarId();
+            Set<LocalDate> changedDates = personalEventService.syncWithGoogleEvents(events, user,
+                googleCalendarId);
 
-        sseEmitters.sendToUser(user.getId(), changedDates);
+            sseEmitters.sendToUser(user.getId(), changedDates);
+        } catch (Exception e) {
+            log.error("Failed to sync personal events", e);
+        }
     }
 }
