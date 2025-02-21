08c49bfbeeb15722390f0e75a0b18358ec4d7f9c
kwon204 <32539398+kwon204@users.noreply.github.com>
2025-02-21 12:54:43 +0900

    [BE-Fix] OUT_OF_RANGE의 기준 변경 (#256)

--- backend/src/main/java/endolphin/backend/domain/personal_event/dto/PersonalEventWithStatus.java
+++ backend/src/main/java/endolphin/backend/domain/personal_event/dto/PersonalEventWithStatus.java
@@ -18,7 +18,7 @@ public record PersonalEventWithStatus(
         if (personalEvent.getIsAdjustable()) {
             status = PersonalEventStatus.ADJUSTABLE;
         }
-        if (personalEvent.getEndTime().isBefore(startTime) || personalEvent.getStartTime().isAfter(endTime)) {
+        if (!personalEvent.getEndTime().isAfter(startTime) || !personalEvent.getStartTime().isBefore(endTime)) {
             status = PersonalEventStatus.OUT_OF_RANGE;
         }
         return new PersonalEventWithStatus(personalEvent.getId(), personalEvent.getStartTime(),
