e03ebaf630bdbecab4851b249359c2e038385ed0
이현영 <89445100+hamo-o@users.noreply.github.com>
2025-02-22 12:40:17 +0900

    [FE-Feat] 초대장 페이지 OG태그 설정 (#269)

--- frontend/src/routes/_main/discussion/invite/$id.tsx
+++ frontend/src/routes/_main/discussion/invite/$id.tsx
@@ -1,21 +1,51 @@
-import { createFileRoute, useParams } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 
+import { invitationQueryOption } from '@/features/discussion/api/queryOptions';
 import GlobalNavBar from '@/layout/GlobalNavBar';
 import DiscussionInvitePage from '@/pages/DiscussionPage/DiscussionInvitePage';
 
 const DiscussionInvite = () => {
-  const { id } = useParams({ from: '/_main/discussion/invite/$id' });
+  const { invitation } = Route.useLoaderData();
   return  (
     <>
       <GlobalNavBar>
         <GlobalNavBar.MyCalendarLink />
         <GlobalNavBar.NewDiscussionLink />
       </GlobalNavBar>
-      <DiscussionInvitePage discussionId={Number(id)} />
+      <DiscussionInvitePage invitation={invitation} />
     </>
   );
 };
 
 export const Route = createFileRoute('/_main/discussion/invite/$id')({
+  loader: async ({ params: { id }, context }) => {
+    const invitation = await context.queryClient.fetchQuery(invitationQueryOption(Number(id)));
+    return { invitation };
+  },
   component: DiscussionInvite,
+  head: ({ loaderData }) => ({
+    meta: [
+      {
+        title: `언제만나 - ${loaderData.invitation.host}님의 초대장`,
+      },
+      {
+        name: 'og:title',
+        content: `언제만나 - ${loaderData.invitation.host}님의 초대장`,
+      },
+      { 
+        name: 'description', 
+        content: 
+        `${loaderData.invitation.title} 일정에 ${loaderData.invitation.host}님이 초대했어요.\n지금 참여해 보세요!🗓️`, 
+      },
+      { 
+        name: 'og:description', 
+        content: 
+        `${loaderData.invitation.title} 일정에 ${loaderData.invitation.host}님이 초대했어요.\n지금 참여해 보세요!🗓️`, 
+      },
+      {
+        name: 'og:image',
+        content: '/images/assets/calendar.webp',
+      },
+    ],
+  }),
 });
