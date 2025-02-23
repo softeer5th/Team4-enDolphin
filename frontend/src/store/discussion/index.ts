e4930af3189f2e3df23d00c06d3ae58861883544
hamo-o <yheel369@gmail.com>
2025-02-24 07:41:09 +0900

    fix: 탭 순위로 보기 먼저 보여주기

--- frontend/src/store/discussion/index.ts
+++ frontend/src/store/discussion/index.ts
@@ -1,5 +1,5 @@
 import { atom } from 'jotai';
 
 export const checkboxAtom = atom<number[] | null>(null);
-export const tabAtom = atom<'calendar' | 'rank'>('calendar');
+export const tabAtom = atom<'calendar' | 'rank'>('rank');
 export const segmentAtom = atom<'participant' | 'time'>('participant');
