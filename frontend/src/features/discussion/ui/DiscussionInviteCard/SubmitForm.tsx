d875a5879ded2e6d8f13d32aeb0d9afbb9b6ff24
이재영 <dioo1461@gmail.com>
2025-02-28 00:31:18 +0900

    fix: 429 too many request 에러일 때만 타이머를 재설정하도록 수정

--- frontend/src/features/discussion/ui/DiscussionInviteCard/SubmitForm.tsx
+++ frontend/src/features/discussion/ui/DiscussionInviteCard/SubmitForm.tsx
@@ -5,11 +5,11 @@ import Button from '@/components/Button';
 import { Flex } from '@/components/Flex';
 import { addNoti } from '@/store/global/notification';
 import { MINUTE_IN_MILLISECONDS } from '@/utils/date';
+import { HTTPError } from '@/utils/error';
 
 import { useInvitationJoinMutation } from '../../api/mutations';
 import { inputStyle } from './index.css';
 import TimerButton from './TimerButton';
-;
 
 interface SubmitFormProps {
   discussionId: number;
@@ -26,29 +26,22 @@ const SubmitForm = ({ discussionId, requirePW, canJoin, unlockDateTime }: Submit
   const handleJoinClick = () => {
     mutate(
       { body: { discussionId, password: password === '' ? undefined : password } },
-      {
-        onSuccess: (data) => {
-          if (data.isSuccess) {
-            navigate({ to: '/discussion/$id', params: { id: discussionId.toString() } });
-          } else {
-            addNoti({ type: 'error', title: `비밀번호가 일치하지 않습니다 - ${data.failedCount}회 시도` });
-          }
-        },
-        onError: () => {
-          setUnlockDT(new Date(Date.now() + 5 * MINUTE_IN_MILLISECONDS)); 
-        },
+      { onSuccess: (data) => {
+        if (data.isSuccess) {
+          navigate({ to: '/discussion/$id', params: { id: discussionId.toString() } });
+        } else {
+          addNoti({ type: 'error', title: `비밀번호가 일치하지 않습니다 - ${data.failedCount}회 시도` });
+        }
       },
-    );
+      onError: (error: Error) => {
+        if (error instanceof HTTPError && error.isTooManyRequestsError()) {
+          setUnlockDT(new Date(Date.now() + 5 * MINUTE_IN_MILLISECONDS)); 
+        }
+      } });
   };
-  return(
+  return (
     <Flex align='flex-end' gap={500}>
-      {requirePW && canJoin && (
-        <input
-          className={inputStyle}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder='숫자 4~6자리 비밀번호'
-        />
-      )}
+      {requirePW && canJoin && <PasswordInput onChange={setPassword} />}
       <JoinButton
         canJoin={canJoin}
         initialUnlockDateTime={unlockDT}
@@ -58,6 +51,14 @@ const SubmitForm = ({ discussionId, requirePW, canJoin, unlockDateTime }: Submit
     </Flex>
   ); 
 };
+
+const PasswordInput = ({ onChange }: { onChange: (password: string) => void }) => (
+  <input
+    className={inputStyle}
+    onChange={(e) => onChange(e.target.value)}
+    placeholder='숫자 4~6자리 비밀번호'
+  />
+);
 
 const JoinButton = ({ canJoin, onClick, initialUnlockDateTime, onTimeEnd }: { 
   canJoin: boolean;
